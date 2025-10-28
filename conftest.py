8291a2890d5afd5286412ffa7902eb86a805b4e0
SYwatcher <alg0r@bk.ru>
2025-10-28 20:30:46 +0300

    create conftest.py with fixtures

---
+++ conftest.py
@@ -0,0 +1,41 @@
+from datetime import timedelta, datetime
+from string import digits
+from time import strftime
+from faker import Faker
+from core.clients.api_client import ApiClient
+import pytest
+
+@pytest.fixture(scope="session")
+def api_client():
+    client = ApiClient()
+    client.auth()
+    return client
+
+@pytest.fixture
+def booking_dates():
+    today = datetime.today()
+    checkin_date = today + timedelta(days=10)
+    checkout_date = checkin_date + timedelta(days=5)
+
+    return {
+        "checkin": checkin_date.strftime("%Y-%m-%d"),
+        "checkout": checkout_date.strftime("%Y-%m-%d")
+    }
+
+@pytest.fixture()
+def generate_random_booking_data(booking_dates):
+    faker = Faker()
+    firstname = faker.first_name()
+    lastname = faker.last_name()
+    totalprice = faker.random_number(digits=3)
+    depostpaid = faker.boolean()
+    additionalneeds = faker.sentence()
+
+    data = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "totalprice": totalprice,
+        "depostpaid": depostpaid,
+        "additionalneeds": additionalneeds
+    }
+    return data
