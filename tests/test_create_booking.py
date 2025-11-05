be73d6de99616c62a74e2a2a997bc321ab354fa8
SYwatcher <alg0r@bk.ru>
2025-11-05 22:50:26 +0300

    убрал уже ненужную переменную

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -8,8 +8,7 @@ from core.clients.api_client import ApiClient
 allure.feature("Test create booking")
 allure.story("Create booking")
 def test_create_booking(api_client, generate_random_booking_data):
-    booking_data = generate_random_booking_data
     with allure.step("Create a new booking"):
-        response = api_client.create_booking(booking_data)
+        response = api_client.create_booking(generate_random_booking_data)
     with allure.step("Return id booking"):
         assert isinstance(response["bookingid"], int)
